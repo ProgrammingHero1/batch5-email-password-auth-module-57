cfd7a93a1fe6c878d81f1d3efecdf78203c9f65f
Programming Hero <programming.hero1@gmail.com>
2022-03-31 20:58:09 -0500

    simple form behavior

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import app from "./firebase.init";
 const auth = getAuth(app)
 
 function App() {
+
+  const handleEmailBlur = event =>{
+    console.log(event.target.value);
+  }
+
+  const handlePasswordBlur = event =>{
+    console.log(event.target.value);
+  }
+
+  const handleFormSubmit = event =>{
+    console.log('form submitted');
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Login" />
       </form>
     </div>
   );
