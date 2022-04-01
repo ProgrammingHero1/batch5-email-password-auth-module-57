11546db5643a387141a90d8bb1ec5fcc2ed29a64
Programming Hero <programming.hero1@gmail.com>
2022-03-31 23:25:28 -0500

    toggle registration and login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import app from "./firebase.init";
@@ -9,6 +9,9 @@ import { useState } from "react";
 const auth = getAuth(app)
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -20,37 +23,83 @@ function App() {
     setPassword(event.target.value);
   }
 
+  const handleRegisteredChange = event =>{
+    setRegistered(event.target.checked)
+  }
+
   const handleFormSubmit = event => {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(result =>{
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error =>{
-      console.error(error);
-    })
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password Should contain at least one special character');
+      return;
+    }
+    setValidated(true);
+    setError('');
+
+    if(registered) {
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result =>{
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error =>{
+        console.error(error);
+        setError(error.message);
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setEmail('');
+        setPassword('')
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+      })
+    }
+
+    
     event.preventDefault();
   }
 
   return (
     <div>
       <div className="registration w-50 mx-auto mt-5">
-        <h2 className="text-primary">Please Register!!</h2>
-        <Form onSubmit={handleFormSubmit}>
+        <h2 className="text-primary">Please { registered ? 'Login': 'Register'}!!</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
           </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered?" />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
           <Button variant="primary" type="submit">
-            Submit
+            {registered ? 'Login':  'Register'}
           </Button>
         </Form>
       </div>
