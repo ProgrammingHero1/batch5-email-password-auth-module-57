d7827c8f1014a87b79cc792577c9b7523efe3635
Programming Hero <programming.hero1@gmail.com>
2022-03-31 22:09:21 -0500

    set email and password auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import app from "./firebase.init";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
 const auth = getAuth(app)
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleEmailBlur = event => {
-    console.log(event.target.value);
+    setEmail(event.target.value);
   }
 
   const handlePasswordBlur = event => {
-    console.log(event.target.value);
+    setPassword(event.target.value);
   }
 
   const handleFormSubmit = event => {
-    console.log('form submitted');
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result =>{
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error =>{
+      console.error(error);
+    })
     event.preventDefault();
   }
 
